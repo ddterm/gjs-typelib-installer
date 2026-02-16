50bcf6614a64346b81433e4aca09d2894a50df94
Aleksandr Mezin <mezin.alexander@gmail.com>
2026-02-16 19:42:32 +0200

    tests: log actual commands

--- tests/e2e-tests.sh
+++ tests/e2e-tests.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: MIT
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-SRC_DIR="$(dirname "$SCRIPT_DIR")"
+SRC_DIR="$(dirname -- "$SCRIPT_DIR")"
 BUILD_DIR="$SRC_DIR/build"
 
 if [ "$(id -un)" != testuser ]; then
@@ -17,7 +17,7 @@ startgroup() {
     if [ -n "$GITHUB_ACTIONS" ]; then
         echo "::group::$*"
     else
-        echo "---------------- $* ----------------"
+        echo "----- $* -----"
     fi
 }
 
@@ -25,21 +25,25 @@ endgroup() {
     if [ -n "$GITHUB_ACTIONS" ]; then
         echo '::endgroup::'
     else
-        echo "------------------------------------"
+        echo "-----"
     fi
 }
 
-doconfigure() {
-    startgroup Configure "$@"
-    meson setup --reconfigure "-Dtypelibs=[$(printf '"%s",' "$@")]" -Dtests=true "$BUILD_DIR" "$SRC_DIR"
-    ninja -C "$BUILD_DIR" all meson-test-prereq
+dorun() {
+    startgroup "$@"
+    local rc=0
+    "$@" || rc=$?
     endgroup
+    return $rc
+}
+
+doconfigure() {
+    dorun meson setup --reconfigure "$@" -Dtests=true "${BUILD_DIR#"$PWD/"}" "${SRC_DIR#"$PWD/"}"
+    dorun ninja -C "${BUILD_DIR#"$PWD/"}" all meson-test-prereq
 }
 
 dotest() {
-    startgroup e2e-test "$@"
-    meson test -C "$BUILD_DIR" --no-rebuild --print-errorlogs --wrapper "$SCRIPT_DIR/yes.expect" --logbase "$(printf '%s-' "$@")" --test-args "$(printf '%q ' "$@")" e2e-test
-    endgroup
+    dorun meson test -C "${BUILD_DIR#"$PWD/"}" --no-rebuild --print-errorlogs --wrapper "$SCRIPT_DIR/yes.expect" --logbase "$(printf '%s-' "$@")" --test-args "$(printf '%q ' "$@")" e2e-test
 }
 
 . /etc/os-release
@@ -57,26 +61,26 @@ esac
 
 set -e
 
-doconfigure cairo-1.0
+doconfigure '-Dtypelibs=["cairo-1.0"]'
 
 [ "$SKIP_FREEDESKTOP" = "1" ] || dotest cairo-1.0
 
-doconfigure GLibUnix-2.0
+doconfigure '-Dtypelibs=["GLibUnix-2.0"]'
 
 [ "$SEPARATE_GLIBUNIX" != "1" ] || dotest GLibUnix-2.0
 
-doconfigure Pango-1.0 Gtk-3.0 Gdk-3.0 Handy-1
+doconfigure '-Dtypelibs=["Pango-1.0","Gtk-3.0","Gdk-3.0","Handy-1"]'
 
 [ "$SKIP_GTK3" = "1" ] || dotest Pango-1.0
 [ "$SKIP_GTK3" = "1" ] || dotest Gtk-3.0 Gdk-3.0
 dotest Handy-1
 
-doconfigure Gtk-4.0 Gdk-4.0 Adw-1
+doconfigure '-Dtypelibs=["Gtk-4.0","Gdk-4.0","Adw-1"]'
 
 dotest Gtk-4.0 Gdk-4.0
 dotest Adw-1
 
-doconfigure Vte-2.91 Vte-3.91
+doconfigure '-Dtypelibs=["Vte-2.91","Vte-3.91"]'
 
 if [ "$VTE4_WRONG_PACKAGE" = "1" ]; then
 	dotest Vte-2.91 Vte-3.91
@@ -85,11 +89,9 @@ else
 	dotest Vte-3.91
 fi
 
-startgroup Configure default
-rm -rf "$BUILD_DIR/"{meson-private,meson-info}
-meson setup --reconfigure -Dtests=true "$BUILD_DIR" "$SRC_DIR"
-endgroup
+# reset options, keep test logs
+dorun rm -rvf "${BUILD_DIR#"$PWD/"}/"{meson-private,meson-info}
 
-startgroup Test
-meson test -C "$BUILD_DIR" --verbose --print-errorlogs test
-endgroup
+doconfigure
+
+dorun meson test -C "${BUILD_DIR#"$PWD/"}" --no-rebuild --verbose --print-errorlogs test
